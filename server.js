0288d255a60e16247883d0071fa8bb72d69cc23c
Nate <nathan.smeltzer@gmail.com>
2021-03-13 12:48:00 -0500

    start subscription

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
 const Router = require('koa-router');
+const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
 dotenv.config();
 
@@ -33,12 +34,14 @@ app.prepare().then(() => {
 
   server.use(
     createShopifyAuth({
-      afterAuth(ctx) {
+      async afterAuth(ctx) {
         // console.log(`ctx.state.shopify: ${ctx.state.shopify}`);
-        const { shop, scope } = ctx.state.shopify;
+        const { shop, scope, accessToken } = ctx.state.shopify;
         ACTIVE_SHOPIFY_SHOPS[shop] = scope;
 
-        ctx.redirect(`/?shop=${shop}`);
+        const returnUrl = `https://${Shopify.Context.HOST_NAME}?shop=${shop}`;
+        const subscriptionUrl = await getSubscriptionUrl(accessToken, shop, returnUrl);
+        ctx.redirect(subscriptionUrl);
       },
     }),
   );
